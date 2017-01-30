e64f462dd8236c0f7824fe71b54c7726fcbd6f8d
Carlos Romano Pereira da Silva <carlos.silva@mvrec.local>
2017-01-30 09:16:17 -0300

    Finalizando aula de diretivas

--- src/app/app.directive.js
+++ src/app/app.directive.js
@@ -2,6 +2,13 @@ angular
 	.module('app')
 	.directive('appDirective', function() {
 		return {
-			template: '<h1>Minha Diretiva</h1>'
+			template: '<h1>Minha Diretiva</h1>',
+			scope: {
+				name: '@',
+				lastname: '@'
+			},
+			link: function($scope, $element, $attr) {
+				console.log($scope);
+			}
 		}
 	});
